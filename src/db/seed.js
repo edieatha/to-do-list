7c1bd6946509b4055fb09f84fa59ce6016dcc2f6
edieatha <edieatha@gmail.com>
2020-10-09 18:59:03 +0300

    Additional linter errors fixed

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,20 +1,18 @@
-import Task from '../models/task';
-import Project from '../models/project';
 import projectController from '../controllers/project_controller';
 import taskController from '../controllers/task_controller';
 
 const db = () => {
-  if("project" in localStorage === false){
+  if ('project' in localStorage === false) {
     projectController.create('HTML');
   }
-  if("task" in localStorage === false){
-    let idProject = projectController.all()[0].id;
+  if ('task' in localStorage === false) {
+    const idProject = projectController.all()[0].id;
     taskController.create('Bootstrap',
-    'Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée àtitre provisoire pour calibrer',
-    '2020-10-22',
-    2,
-    idProject);
+      'Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée àtitre provisoire pour calibrer',
+      '2020-10-22',
+      2,
+      idProject);
   }
-}
+};
 
 export default db;
